5e0719d0ec0f579c923046700f6c4552798e1bc4
QuinnMarshal <marshallq4711@gmail.com>
2024-05-09 12:16:01 -0700

    added time entry view to views.py

--- timetracker_backend/timetracker_api/views.py
+++ timetracker_backend/timetracker_api/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth import get_user_model, login, logout
 from rest_framework.views import APIView
 from rest_framework.authtoken.models import Token
 from rest_framework import status, permissions
-from .serializers import UserRegistrationSerializer, UserLoginSerializer, UserSerializer
+from .serializers import UserRegistrationSerializer, UserLoginSerializer, UserSerializer, TimeEntrySerializer
 from rest_framework.response import Response
 from rest_framework.authentication import SessionAuthentication, TokenAuthentication
 
@@ -11,6 +11,7 @@ from rest_framework.authentication import SessionAuthentication, TokenAuthentica
 # The post() method is created to register a new user with the validated data
 class UserRegister(APIView):
     permission_classes = [permissions.AllowAny]
+    
     def post(self, request):
         serializer = UserRegistrationSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
@@ -26,6 +27,7 @@ class UserRegister(APIView):
 class UserLogin(APIView):  
     permission_classes = [permissions.AllowAny]
     authentication_classes = [SessionAuthentication, TokenAuthentication]
+    
     def post(self, request):
         data = request.data
         serializer = UserLoginSerializer(data=data)
@@ -40,6 +42,7 @@ class UserLogin(APIView):
 class UserLogout(APIView):
     permission_classes = [permissions.AllowAny]
     authentication_classes = ()
+    
     def post(self, request):
         logout(request)
         return Response(status=status.HTTP_200_OK)
@@ -52,3 +55,22 @@ class UserList(APIView):
     def get(self, request):
         serializer = UserSerializer(request.user)
         return Response({"user": serializer.data}, status=status.HTTP_200_OK)
+    
+# TimeEntryList class is created to get the time entry data
+# The get() method is created to get all the time entries of a user
+# The post() method is created to create a new time entry for a user
+class TimeEntryList(APIView):
+    authentication_classes = [SessionAuthentication, TokenAuthentication]
+    permission_classes = [permissions.AllowAny]
+    
+    def get(self, request):
+        serializer = TimeEntrySerializer(request.user.timeentry_set.all(), many=True)
+        return Response({"timeentry": serializer.data}, status=status.HTTP_200_OK)
+    
+    def post(self, request):
+        serializer = TimeEntrySerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save(user=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
