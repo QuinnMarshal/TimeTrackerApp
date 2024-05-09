53d556e4882c251a6383f5a380cc1a70e1e97a6b
QuinnMarshal <marshallq4711@gmail.com>
2024-05-09 12:38:53 -0700

    added changes to index.js to route the app and add the time entry context provider

--- timetracker_frontend/src/TimeEntryContext.js
+++ timetracker_frontend/src/TimeEntryContext.js
@@ -1,3 +1,4 @@
 import React from 'react';
 
+// Create a context for the time entry data
 export const TimeEntryContext = React.createContext();
--- timetracker_frontend/src/index.js
+++ timetracker_frontend/src/index.js
@@ -3,13 +3,30 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { TimeEntryContext } from './TimeEntryContext';
+import { useState } from 'react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+function Main() {
+
+  const [timeEntries, setTimeEntries] = useState([]);
+
+  return (
+    <React.StrictMode>
+      <Router>
+        <TimeEntryContext.Provider value={{ timeEntries, setTimeEntries }}>
+          <Routes>
+            <Route path="/" element={<App />} />
+          </Routes>
+        </TimeEntryContext.Provider>
+      </Router>
+    </React.StrictMode>
+  );
+}
+
+root.render(<Main />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
