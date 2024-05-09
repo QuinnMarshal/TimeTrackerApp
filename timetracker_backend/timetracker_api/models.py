53535776d068b9a72d61166060a0e02521b5fbf2
QuinnMarshal <marshallq4711@gmail.com>
2024-05-09 10:25:19 -0700

    added models to set up user login page

--- timetracker_backend/timetracker_api/models.py
+++ timetracker_backend/timetracker_api/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
-# Create your models here.
+# AppUserManager and AppUser classes are created to override the default User model of Django
+# AppUserManager class is created to manage the creation of users
+class AppUserManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        if not email:
+            raise ValueError("The Email field must be set")
+        if not password:
+            raise ValueError("The Password field must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email)
+        user.set_password(password)
+        user.save()
+        return user
+    
+    def create_superuser(self, email, password=None):
+        user = self.create_user(email, password)
+        user.is_superuser = True
+        user.save()
+        return user
+    
+# AppUser class is created to add the user_id field
+class AppUser(AbstractBaseUser, PermissionsMixin):
+    user_id = models.AutoField(primary_key=True)
+    email = models.EmailField(max_length=50, unique=True)
+    username = models.CharField(max_length=50)
+    
+    objects = AppUserManager()
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+    
+    def __str__(self):
+        return self.username
